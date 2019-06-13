614581952dece9800c7ba7155e285515cc5dbe0d
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-13 04:18:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,6 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
@@ -22,28 +21,24 @@ class Application
           resp.write "#{cart_item}\n"
         end
       end
-      elsif req.path.match(/add/)
-        item = req.params["item"]
-        if @@items.include?(item)
-          @@cart << item
-          resp.write "added #{item}"
-        else
-          resp.write "We don't have that item"
-        end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
       else
-        resp.write "Path Not Found"
+        resp.write "We don't have that item"
       end
-
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
     else
-      return "Couldn't find #{search_term}"
+      resp.write "Path Not Found"
+    end
+    resp.finish
+    end
+    def handle_search(search_term)
+      if @@items.include?(search_term)
+        return "#{search_term} is one of our items"
+      else
+        return "Couldn't find #{search_term}"
+      end
     end
   end
-
-  def
-end
